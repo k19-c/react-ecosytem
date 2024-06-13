d1e425e176234025184b05319eecd93e34a3a7f5
KalalChandu <kalalshannu19@gmail.com>
2024-06-13 10:59:01 +0530

    Implementing:react with CRA and Vite

---
+++ react.js
@@ -0,0 +1,38 @@
+/*
+
+--> React is a Library
+
+--> Framework is like Military and Library is like Cool Dude
+
+--> Framework is Really strict how the file name should be, the Naming Convention, Folder Structure
+
+--> BAAS Apps Backend as a service i.e:- Social Media Apps, Ecommerce Apps
+
+--> Class based Components are Legacy based codes
+
+--> React is not a complete solution in most of the cases
+
+--> No SEO, Browser Render of JS , No Routing .
+
+--> Framework:- Gatsby, Remix, Next Js you can prefer on top of React
+
+
+*/  /* 
+ 
+* Scripts 
+
+--> you can see there are lot of files which got Injected when inspect the browser and see in the Header part
+
+--> React-scripts will do the behind screen magic . while running the Application or building the Application .
+
+--> since the Application is run by react-scripts It Automatically finds the index.html and InJects the Javascript file.
+
+-->  there is a Browser DOM and React DOM known as a Virtual DOM
+
+--> It tries to mimick the Original DOM and whatever the updates we make it first goes into the virtual DOM
+
+--> Any updates happen in the Virtual DOM are pushed in to the Real DOM
+
+--> 
+
+*/
---
+++ vite.js
@@ -0,0 +1,13 @@
+/*
+
+Benifits of using Vite + React
+
+* Instant Server Start On demand file serving over native ESM, no bundling required!
+
+* Lightning Fast HMR :-  Hot Module Replacement (HMR) that stays fast regardless of app size.
+
+* 
+
+
+
+*/
